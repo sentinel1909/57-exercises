fb9edd5dda7fd7fedd436bba85861f10b63e9540
Jeff Mitchell <sentinel1909@protonmail.com>
2022-04-23 22:02:44 -0700

    Solved exercise #1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # 57-exercises
- A repo documenting my journey to do 57 programming exercises in Rust
+A repo documenting my journey to do 57 programming exercises in Rust.
+
+## Exercise 1: Saying Hello
+### Problem Statement:
+The "Hello, World" program is the first program you learn to write in many languages, but doesn't involve any input.
+So create a program that prompts for your name and prints a greeting using your name.
+
+### Example Output:
+What is your name? Jeff
+Hello, Jeff, nice to meet you!
+
+### Constraint:
+Keep the input, string concatenation, and output separate.
+
+### Solution:
+To run:
+
+```
+cd say-hello
+cargo run
+```
--- say-hello/src/main.rs
+++ say-hello/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+// Problem Statement:
+// The "Hello, World" program is the first program you learn to write in many languages, but doesn't involve any input.
+// So create a program that prompts for your name and prints a greeting using your name.
+
+// Example Output:
+// What is your name? Jeff
+// Hello, Jeff, nice to meet you!
+
+// Constraint:
+// Keep the input, string concatenation, and output separate.
+
+use std::io::{self, Write};
+
+/// function that retrieves user input
+fn get_name() -> String {
+    print!("What is your name? ");
+    io::stdout().flush().unwrap();
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    buffer
 }
+/// function that builds the output string
+fn build_greeting(name: String) -> String {
+    let mut greeting = "Hello, ".to_owned();
+    greeting.push_str(&name[..].trim());
+    greeting.push_str(", nice to meet you!");
+    greeting
+}
+/// function that displays the output string 
+fn print_greeting(greeting: String) {
+    println!("{}", greeting);
+}
+/// main program
+fn main() {
+    print_greeting(build_greeting(get_name()));
+
+}
+
+// tests
+// none (program can be tested trivially by running it)
